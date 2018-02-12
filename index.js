21e653f09f43bc047cbba0e7eb180b1b740eb9b6
RahulDhiman93 <rsdm34@gmail.com>
2018-02-12 16:12:29 +0530

    header improvement

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 const App = () => (
-   <Text>hello dfkjbvkjfbkjfb
-   sfbkjfsnbjkfnb
-   sbkjbnkjbkb
-   sdbjksdbnkbnkdsb
-   bnsjkdbndskbsb</Text>
+   <Header />
 );
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+    <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+
+const styles = {
+  viewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F8F8F8',
+    height: 80,
+    padding: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.6,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
